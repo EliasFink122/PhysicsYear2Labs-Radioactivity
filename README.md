f85afc90b0e15f3e6926386a08a8b40a3de9b3dd
Elias Fink <elias.fink@gmx.de>
2023-11-14 12:17:25 +0000

    Added first feature to quickly plot histograms.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PhysicsYear2Labs-Radioactivity
 Code for radioactivity lab.
+
+- quick_plot.py to plot histogram of data from DAQ output using "python quick_plot.py filename.txt nbins" in command line.
---
+++ quick_plot.py
@@ -0,0 +1,21 @@
+"""
+Plot histogram of DAQ output.
+"""
+#############################################################################
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+#############################################################################
+
+def main():
+    data = np.loadtxt(str(sys.argv[1])) # load data file
+
+    plt.hist(data, int(sys.argv[2])) # create histogram
+    plt.xlabel("Counts per cycle")
+    plt.ylabel("Number of cycles")
+    plt.title("Histogram of output from DAQ")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
